ae04188e75bf1cdc09864c4692b9b491957f8e9b
don79is <don79is@gmail.com>
2017-08-31 14:03:34 +0300

    add jason file fix code

--- app/Http/Controllers/DDGrabingController.php
+++ app/Http/Controllers/DDGrabingController.php
@@ -11,6 +11,7 @@ class DDGrabingController extends Controller
         $info = json_decode(file_get_contents(storage_path('data.json')), true);
         dd($info);
     }
+
     public function alldata()
     {
 
@@ -30,11 +31,33 @@ class DDGrabingController extends Controller
 //        dd($prices);
 
 
+        for ($day = 1; $day < 29; $day++) {
 
-        for ($day = 1; $day < 31; $day++) {
-            dd($day);
+            $pricesRound = round($prices[0]['price'][$day - 1]);
 
+            if ($prices[0]['price'][$day - 1] == '&nbsp;') {
+            } else {
+                $info = new Copycat;
+                $info->setCURL(array(
+                    CURLOPT_RETURNTRANSFER => 1,
+                    CURLOPT_CONNECTTIMEOUT => 5,
+                    CURLOPT_HTTPHEADER, "Content-Type: text/html; charset=iso-8859-1",
+                    CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17',
+                ));
+
+                $info->match(array(
+                    'date' => '!<td class="leftcell" colspan="2">(.*?)&nbsp;(.*?)<\/td>!',
+                    'departure' => '!<td class="depdest" title="Flight DY1072"><div class="content emphasize">(.*?)<\/div><\/td>!',
+                    'arrival' => '!<td class="arrdest"><div class="content emphasize">(.*?)<\/div><\/td>!',
+                    'details' => '!<td class="duration"><div class="content">Duration:(.*?)<\/div><\/td>!',
+                    'price' => '!<td align="right" valign="bottom" class="rightcell totalfarecell" nowrap="nowrap">(.*?)<\/td>!',
+                    'tax' => '!<td class="rightcell emphasize" align="right" valign="bottom">(.*?)<\/td>!'
+                ))->URLs('https://www.norwegian.com/en/booking/flight-tickets/select-flight/?D_City=OSL&A_City=RIX&TripType=1&D_SelectedDay=' . $day . '&D_Day=' . $day . '&D_Month=201710&R_SelectedDay=' . $day . '&R_Day=01&R_Month=201710&dFare=' . $pricesRound . '&IncludeTransit=false&AgreementCodeFK=-1&CurrencyCode=EUR');
+                $data[] = $info->get();
+                file_put_contents(storage_path('data.json'), json_encode($data));
+            }
         }
+        dd($data);
     }
 }
 
--- app/Http/Controllers/SASController.php
+++ app/Http/Controllers/SASController.php
@@ -24,8 +24,7 @@ class SASController extends Controller
         ));
 
         $file->matchALL(array(
-            'day' => '!<span class="date">(.*?)<\/span>!',
-            'price' => '!<span class="price"><input type="hidden" id="recoHidden_0_0_PREMB" value="5"> <span class="currency">€<\/span><span class="number tobeupdated1" id="price_0_0_PREMB" data-price="">(.*?)<\/span> <\/span>',
+            'price' => '!<span class="price">     <span class="currency">(.*?.*?)<\/span>   <\/span>!',
         ))->URLs('https://book.flysas.com/pl/SAS/wds/Override.action?SO_SITE_EXT_PSPURL=https://classic.sas.dk/SASCredits/SASCreditsPaymentMaster.aspx&SO_SITE_TP_TPC_POST_EOT_WT=50000&SO_SITE_USE_ACK_URL_SERVICE=TRUE&WDS_URL_JSON_POINTS=ebwsprod.flysas.com%2FEAJI%2FEAJIService.aspx&SO_SITE_EBMS_API_SERVERURL=https%3A%2F%2F1aebwsprod.flysas.com%2FEBMSPointsInternal%2FEBMSPoints.asmx&WDS_SERVICING_FLOW_TE_SEATMAP=TRUE&WDS_SERVICING_FLOW_TE_XBAG=TRUE&WDS_SERVICING_FLOW_TE_MEAL=TRUE');
 
         $info[] = $file->get();
